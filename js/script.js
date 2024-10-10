47f40214e28e622a6cda84ccaf59a019879a0239
AskatAsh <abdulaskat@gmail.com>
2024-10-10 23:36:54 +0600

    used fetch api to fetch all pets data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// 1. fetch pets category and display in ui
 const categories_container = document.getElementById("categories");
 const categories_url =
   "https://openapi.programming-hero.com/api/peddy/categories";
@@ -5,14 +6,12 @@ const categories_url =
 // display pets category
 function displayCategories(categories) {
   categories.forEach((category) => {
-    // console.log(category);
     const li = document.createElement("li");
     li.className =
       "flex items-center justify-center gap-3 p-5 border-2 border-light1 rounded-xl sm:w-full cursor-pointer hover:bg-light1";
     li.innerHTML = `
         <img class="w-8" src="${category?.category_icon}" alt="${category?.category} icon">
         <span class="text-xl sm:text-2xl text-dark1 font-black">${category?.category}s</span>`;
-    console.log(li);
     categories_container.appendChild(li);
   });
 }
@@ -21,7 +20,22 @@ async function getCategoriesData() {
   const categoriesResponse = await fetch(categories_url);
   const categoriesData = await categoriesResponse.json();
   const categories = categoriesData?.categories;
-  console.log(categories);
   displayCategories(categories);
 }
 getCategoriesData();
+
+// 2. fetch all pets data and display in cards ui
+const petsContainer = document.getElementById("pets-cards");
+const all_pets_url = 'https://openapi.programming-hero.com/api/peddy/pets';
+
+function displayPetsData(pets){
+    console.log(pets)
+}
+// fetch pets data using api
+async function getPetsData(){
+    const petsResponse = await fetch(all_pets_url);
+    const petsData = await petsResponse.json();
+    const pets = petsData.pets;
+    displayPetsData(pets);
+}
+getPetsData();
